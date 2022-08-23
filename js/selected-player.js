0e88f3c70e33e1e4bffbad9ee25d8e0c6ddc5724
nazmuldip <nazmuldip21@gmail.com>
2022-08-23 15:56:56 +0600

    calculate expence and add  playerName for disabled button

---
+++ js/selected-player.js
@@ -0,0 +1,13 @@
+
+playerName('achraf-hakimi-name', 'achraf-hakimi-select-btn');
+playerName('gianluigi-name', 'gianluigi-select-btn');
+playerName('juan-bernat', 'juan-bernat-select-btn');
+playerName('kylian-mbappe-name', 'kylian-mbappe-select-btn');
+playerName('leandro-paredes-name', 'leandro-paredes-select-btn');
+playerName('marco-verratti-name', 'marco-verratti-select-btn');
+playerName('marquinhos_brazil-name', 'marquinhos_brazil-select-btn');
+playerName('messi-name', 'messi-select-btn');
+playerName('naymer-name', 'naymer-select-btn');
+playerName('ramos-name', 'ramos-select-btn');
+playerName('silva-name', 'silva-select-btn');
+playerName('vitinha-name', 'vitinha-select-btn');
